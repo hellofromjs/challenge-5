c689a79d3e979963e9daaf991e26505c8e262298
Edvinas <edvinas.gudavicius@stud.kitm.lt>
2023-12-14 20:28:13 +0200

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+const convert_form = document.querySelector('#convert-form')
+const years_input = convert_form.querySelector('input')
+const result = document.querySelector('#result')
+
+convert_form.querySelector('button').addEventListener('click', e => {
+	e.preventDefault()
+
+	convert_form.classList.add('hide')
+	result.classList.remove('hide')
+
+	document.querySelector('#output').textContent = `${make_ordinal(years_input.value)} Century`
+})
+
+document.querySelector('#try-again').addEventListener('click', e => {
+	years_input.value = ""
+
+	result.classList.add('hide')
+	convert_form.classList.remove('hide')
+})
+
+function make_ordinal(year) {
+	const century = Math.ceil(parseInt(year) / 100)
+
+	if (century % 10 == 1) {
+		return century + 'st'
+	} else if (century % 10 == 2) {
+		return century + 'nd'
+	} else if (century % 10 == 3) {
+		return century + 'rd'
+	} else {
+		return century + 'th'
+	}
+}
